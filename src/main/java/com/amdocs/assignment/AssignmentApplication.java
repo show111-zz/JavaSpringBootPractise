f71da5dad758ca0d43af467d5753a6f7407832e6
huilee <huilee9999@gmail.com>
2021-09-08 17:24:49 -0400

    setup springboot and H2

---
+++ src/main/java/com/amdocs/assignment/AssignmentApplication.java
@@ -0,0 +1,11 @@
+package com.amdocs.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
+}
